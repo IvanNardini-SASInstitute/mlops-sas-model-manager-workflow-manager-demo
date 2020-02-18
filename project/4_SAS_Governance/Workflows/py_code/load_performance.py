dd0bd7148162980971417de3b983094374806f04
ivnard <ivan.nardini@sas.com>
2020-02-18 06:45:43 +0100

    Performances

--- project/4_SAS_Governance/Workflows/py_code/load_performance.py
+++ project/4_SAS_Governance/Workflows/py_code/load_performance.py
@@ -1,20 +1,33 @@
 
 #Libraries
-
+import glob
 import pandas as pd
 import swat
 
 # Session variable
+_HOST = '172.28.234.57'
+_PORT = '5570'
+_USERNAME = 'sasdemo'
+_PASSWORD = 'Orion123'
+_CASLIB = 'Public'
 
-_USERNAME = 'xxx'
-_PASSWORD = 'xxx'
-_CASLIB = 'xxx'
+_PATH="./Data/performances/*.csv"
 
 
 # Start CAS Session
-cas_session = swat.CAS(caslib = _CASLIB)
+
+cas_session = swat.CAS(_HOST, _PORT, _USERNAME, _PASSWORD)
+
+out = cas_session.serverstatus()
+print(out)
 
 # For each csv, load into caslib Public with 30 secs
+
+for table in glob.glob(_PATH):
+    print("Loading Performance Table...")
+    # df = pd.read_csv(_PATH, sep=',', header=0)
+    # perftbl = cas_session.upload_frame(df, casout=dict(name="input",replace=True,caslib=cas_library))
+
 
 # df = pd.DataFrame(data = [[1,1,1,'HomeImp','ProfExe',1,1,1,1,1,1,1],[_LOAN, _MORTDUE, _VALUE, _REASON, _JOB, _YOJ, _DEROG, _DELINQ, _CLAGE, _NINQ, _CLNO, _DEBTINC]], 
 #   	columns = ["LOAN", "MORTDUE", "VALUE", "REASON", "JOB", "YOJ", "DEROG", "DELINQ", "CLAGE", "NINQ", "CLNO", "DEBTINC"])
